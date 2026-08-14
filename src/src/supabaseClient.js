cf148ecd1ab99abcc44984a6c2a191c9d04789be
Ange-Kakou <langeofficiel@gmail.com>
2026-08-14 15:56:23 +0000

    Delete src directory

--- src/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://uinkrzcfdtqonhkvcxsb.supabase.co";
-const supabaseAnonKey = "sb_publishable_I-y39f0P0gGyjp-9RllW6Q_Smjo3sJ2";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
